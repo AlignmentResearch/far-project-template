612be151ac85c8c8715251d485ef27ecfb589d0f
kellinpelrine <46732724+kellinpelrine@users.noreply.github.com>
2025-12-26 21:53:47 -0700

    Fix broken Jinja

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/constants.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/constants.py
@@ -1,6 +1,6 @@
 """Constants for the {{ cookiecutter.project_name }} package."""
 
 PROJECT_NAME = "{{ cookiecutter.project_name }}"
-PROJECT_SHORT = "{{ [word[0] for word in cookiecutter.project_name.split("_")] }}"
+PROJECT_SHORT = "{{ cookiecutter.project_name.split('_') | map('first') | join('') }}"
 WANDB_ENTITY = "farai"
 WANDB_PROJECT = "{{ cookiecutter.project_name }}"
